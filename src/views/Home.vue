48a282b6018f48563ce8dbc16cced7e7881a0751
Marat Oshmiansky <maratical@gmail.com>
2021-07-20 07:57:44 -0700

    initial commit - not working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <button v-on:click="createMovie()">Create a new movie!</button>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>Title: {{ movie.title }}</h2>
+      <p>Year: ${{ movie.year }}</p>
+      <p>Director: ${{ movie.director }}</p>
+      <p>Plot: ${{ movie.plot }}</p>
+      <!-- <img v-bind:src="movie.image_url" alt="movie.title" /> -->
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "This is movie-vue-app!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All movies:", this.movies);
+      });
+    },
+    createMovie: function () {
+      console.log("Creating movie");
+      var params = {
+        title: "Postcards from the Edge",
+        year: 1990,
+        plot: "An actress struggles with drug addiction while living in the shadow of her famous mother.",
+        director: "Mike Nichols",
+        english: true,
+      };
+      axios
+        .post("http://localhost:3000/movies", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.movies.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
   },
 };
 </script>
