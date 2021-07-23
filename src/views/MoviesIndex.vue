3974a1ba41c738107e8bf4373a68acad66a202ab
Marat Oshmiansky <maratical@gmail.com>
2021-07-23 07:40:46 -0700

    add movies index action, route and link

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="allmovies">
+    <div v-for="movie in movies" :key="movie.id">
+      <router-link v-bind:to="`/movies/${movie.id}`">
+        <h2>Title: {{ movie.title }}</h2>
+        <p>Year: {{ movie.year }}</p>
+        <p>Director: {{ movie.director }}</p>
+        <p>Plot: {{ movie.plot }}</p>
+      </router-link>
+      <button>More info!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Here are movies!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All movies:", this.movies);
+      });
+    },
+  },
+};
+</script>
